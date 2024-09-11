fbdf2ed8eaee45c987003df85726387a5d2402d0
Zuzanna Poskrobko <dataanalyticszuzanna@gmail.com>
2024-09-11 10:32:42 +0000

    Cleaned ads staging models -> renamed campaign_name and cast for ads_cost. Build the models.

--- models/staging/raw/stg_raw__facebook.sql
+++ models/staging/raw/stg_raw__facebook.sql
@@ -12,8 +12,8 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name,
-        ads_cost,
+        campgn_name AS campaign_name,
+        CAST(ads_cost AS FLOAT64) AS ads_cost,
         impression,
         click
 
