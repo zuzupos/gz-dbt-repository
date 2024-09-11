6f725d611d97e52b83d723199ae0d1c6fe87affb
Zuzanna Poskrobko <dataanalyticszuzanna@gmail.com>
2024-09-11 14:41:12 +0000

    Used dbt utils to change union all in int_campaign.sql

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,11 +1,7 @@
-SELECT *
-FROM {{ ref('stg_raw__adwords')}}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__bing')}}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__criteo')}}
-UNION ALL
-SELECT *
-FROM {{ ref('stg_raw__facebook')}}
+{{ dbt_utils.union_relations(
+    relations=[
+        ref('stg_raw__adwords'),
+        ref('stg_raw__bing'),
+        ref('stg_raw__criteo'),
+        ref('stg_raw__facebook')
+]) }}
